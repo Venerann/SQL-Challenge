961d65d433629d0ae08f0757610fb6812a7b774f
Sebastian Alonso <messageboxrtxs@gmail.com>
2023-08-16 20:20:57 -0600

    Created an .sql file for the table_schemata.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # SQL-Challenge
+
+1st commit was to upload the creation of the 'EmployeeSQL' folder in which I moved all relevant CSV files into for the assignment.
---
+++ table_schemata.sql
@@ -0,0 +1,43 @@
+-- table_schemata.sql
+
+CREATE TABLE departments (
+    dept_no CHAR(4) PRIMARY KEY,
+    dept_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE employees (
+    emp_no INT PRIMARY KEY,
+    emp_title_id CHAR(5),
+    birth_date DATE,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    sex CHAR(1),
+    hire_date DATE
+);
+
+CREATE TABLE dept_emp (
+    emp_no INT,
+    dept_no CHAR(4),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE dept_manager (
+    dept_no CHAR(4),
+    emp_no INT,
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    PRIMARY KEY (dept_no, emp_no)
+);
+
+CREATE TABLE salaries (
+    emp_no INT,
+    salary INT,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE titles (
+    title_id CHAR(5) PRIMARY KEY,
+    title VARCHAR(50) NOT NULL
+);
